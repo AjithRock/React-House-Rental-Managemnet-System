8338be84e2e0b3186a55d531dc66615f23e261d4
ajithrock <ajithrocks006@gmail.com>
2020-06-22 00:50:38 +0530

    netlify bug fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "antd/dist/antd.css";
 import "./index.css";
 import App from "./App";
 import "./global";
-import "./App.scss";
+import "./app.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.render(
   <Router>
